ccacc13a635bcca48ce56f198985be660f79d443
MT 矢島 伶美 <remi.yajima@metateam.co.jp>
2024-05-01 16:17:12 +0900

    場合に応じた処理 問１

--- practice1.java
+++ practice1.java
@@ -1,3 +1,5 @@
+impoot
+
 public class Main {
     public static void main(String[] args){
         //クラスを１つ作成
---
+++ practice[if].java
@@ -0,0 +1,14 @@
+public class practice[if].java{
+    public static void main(String[] args){
+        System.out.println(i:"整数値（1~10)を入力してください");
+        int num = new java.util.Scanner(System.in).nextInt();
+
+        if(num<5){
+            System.out.println("5未満の整数" + num +"が入力されました。");
+        } else if(num == 5){
+            System.out.println(i:"5が入力されました。");
+        } else {
+            System.out.pritnln("6以上の整数" + num + "が入力されました。");
+        }
+    }
+}
